f8d8cebd1d618f8e4d611e25d4ed4b0eeb5d95a3
Eduardo Kanno <eduardok@versul.com.br>
2018-01-17 16:33:29 -0200

    teste

---
+++ Gruntfile.js
@@ -0,0 +1,77 @@
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+module.exports = function(grunt) {
+
+    const config = {
+        pkg: grunt.file.readJSON('package.json'),
+        coffeelint: {
+            app: ['cnab/src/*.coffee', 'cnab/test/*.coffee'],
+            options: {
+                configFile: 'coffeelint.json'
+            }
+        },
+        mochaTest: {
+            progress: {
+                options: {
+                    reporter: 'progress',
+                    require: ['coffee-script/register', 'blanket'],
+                    captureFile: 'mochaTest.log',
+                    quiet: false,
+                    clearRequireCache: false
+                },
+                src: ['cnab/test/*.coffee']
+            },
+            spec: {
+                options: {
+                    reporter: 'spec',
+                    require: ['coffee-script/register', 'blanket'],
+                    captureFile: 'mochaTest.log',
+                    quiet: false,
+                    clearRequireCache: false
+                },
+                src: ['cnab/test/*.coffee']
+            }
+        },
+        exec: {
+            cov: `\
+rm -rf cov; \
+mkdir -p cov; \
+./node_modules/.bin/coffee --compile --output cov cnab; \
+./node_modules/mocha/bin/mocha cov/test -r blanket --reporter mocha-cov-reporter --compilers coffee:coffee-script/register --recursive; \
+./node_modules/mocha/bin/mocha cov/test -r blanket --reporter html-cov --compilers coffee:coffee-script/register --recursive > coverage.html`,
+            compile: `\
+node_modules/.bin/coffee --compile --output cnab/ cnab/; \
+node_modules/.bin/coffee --compile --output layout/ layout/; \
+node_modules/.bin/coffee --compile main.coffee; \
+node_modules/.bin/babel cnab --out-dir aaa --copy-files --ignore src/node_modules\
+`
+        },
+        watch: {
+            src: {
+                files: ['**/**/*.coffee', '*.coffee'],
+                tasks: ['lint', 'test', 'coverage', 'compile']
+            },
+            gruntfile: {
+                files: ['Gruntfile.coffee']
+            }
+        }
+    };
+
+    grunt.initConfig(config);
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.registerTask('default', 'Default', function() {
+        grunt.task.run('lint');
+        grunt.task.run('test');
+        // grunt.task.run 'compile'
+        return grunt.task.run('coverage');
+    });
+    grunt.registerTask('lint', ['coffeelint']);
+    grunt.registerTask('test', ['mochaTest:progress']);
+    grunt.registerTask('coverage', ['exec:cov']);
+    return grunt.registerTask('compile', ['exec:compile']);
+};
